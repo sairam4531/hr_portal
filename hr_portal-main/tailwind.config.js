3cc6c14e7e069eeabcd59f176d3906c6129af76f
sairam4531 <tsairam@gnits.ac.in>
2026-03-22 17:18:56 +0530

    fix: remove TS types from tailwind config

--- hr_portal-main/tailwind.config.js
+++ hr_portal-main/tailwind.config.js
@@ -1,8 +1,8 @@
-import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
 export default {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   prefix: "",
   theme: {
     container: {
@@ -63,4 +63,4 @@ export default {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
